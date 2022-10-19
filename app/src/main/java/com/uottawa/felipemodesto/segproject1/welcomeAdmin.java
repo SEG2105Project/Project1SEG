b968ace936dff5d42f4d3d5429164eac3aa43139
jonathancolasante <jonathan.colasante@gmail.com>
2022-10-19 12:28:23 -0400

    Added XML and Class for Welcome Admin

---
+++ app/src/main/java/com/uottawa/felipemodesto/segproject1/welcomeAdmin.java
@@ -0,0 +1,26 @@
+package com.uottawa.felipemodesto.segproject1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+public class welcomeAdmin extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomeadmin);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.adminLogOffID:LogOffID:
+                adminLogOffClick(v);
+                break;
+        }
+    }
+
+    public void adminLogOffClick (View v) {
+        Intent i = new Intent(this, Home.class);
+        startActivity(i);
+    }
+}
