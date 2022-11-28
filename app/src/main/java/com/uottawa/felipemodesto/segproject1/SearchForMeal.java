63638e89012a8d458ac9fb70a9d3d08c23df4fbf
jonathancolasante <jonathan.colasante@gmail.com>
2022-11-27 21:50:57 -0500

    Added onClick method for a button

--- app/src/main/java/com/uottawa/felipemodesto/segproject1/SearchForMeal.java
+++ app/src/main/java/com/uottawa/felipemodesto/segproject1/SearchForMeal.java
@@ -1,11 +1,30 @@
 package com.uottawa.felipemodesto.segproject1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class SearchForMeal extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchformeal);
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginID:
+                findMealsClick(v);
+                break;
+        }
+    }
+
+
+    public void findMealsClick (View v){
+        //code for login button
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+    }
+
+
 }
